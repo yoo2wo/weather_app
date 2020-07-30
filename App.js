b5d8d6a2c3659cee0ce9de8c27d8f6b266a42fc1
yoo2wo <wogud758787@naver.com>
2020-07-30 22:10:42 +0900

    final

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from "react-native";
+import Loading from './Loading';
+import Weather from './Weather';
+import * as Location from "expo-location";
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "d749bca3be8d36d8968e8f4b28fd40e8";
+
+export default class extends React.Component {
+  state = {
+    isLoading : true
+  };
+  //fetch하기  api데이터 
+  getWeather = async(latitude, longitude) =>{
+    const { 
+      data: {
+        main:{temp},
+        weather
+      } 
+    } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main, 
+      temp
+    });
+
+  }
+  getLocation = async() =>{
+    try {
+      await Location.requestPermissionsAsync();
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      
+      //나중에 api를 이용해 날씨를 가져올것이다.
+    } catch (error) {
+      Alert.alert("Can't find you","so sad");
+    }
+    
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition}/>; 
+  }
+  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
